6e647da28deb1838baebb7d578934ce88c62c2cb
Patryk Walczak <patryk.wal@onet.pl>
2020-08-07 22:33:05 +0200

    code formatin and CIs

--- src/file_handler/file_handler.py
+++ src/file_handler/file_handler.py
@@ -1,52 +1,75 @@
 import os
 
-class FileHandler(): 
+
+class FileHandler():
     """
-    The class contain all methods that are required to 
+    The class contain all methods that are required to
     read from, write to and edit a file.
     """
 
-    @classmethod
-    def write_to_file(self, file_path, data): 
-        """ write data to file, replace content if it exists """
+    def write_to_file(self, file_path: str, data: str):
+        """
+        Write data to file,
+        replace content if it exists.
 
+        Args:
+            file_path (str): Path to the file.
+            data (str): Data to save in the file.
+        """
         f = open(file_path, "w")
         f.write(data)
         f.close()
 
-    @classmethod
-    def add_to_file(self, file_path, data):
-        """ add data to file """
+    def add_to_file(self, file_path: str, data: str):
+        """
+        Add data to file.
 
+        Args:
+            file_path (str): Path to the file.
+            data (str): Data to add to the file.
+        """
         f = open(file_path, "a")
         f.write(data)
         f.close()
 
-    @classmethod
-    def read_from_file(self, file_path):
-        """ read data from file """
+    def read_from_file(self, file_path: str):
+        """
+        Read data from file.
 
+        Args:
+            file_path (str): Path to the file.
+
+        Returns:
+            str: The data from the file.
+        """
         f = open(file_path, "r")
         data = f.read()
         f.close()
 
         return data
 
-    @classmethod
-    def create_file(self, file_path, data = None):
-        """ read data from file """
+    def create_file(self, file_path: str, data: str = None):
+        """
+        Create The file.
 
+        Args:
+            file_path (str): Path to the file.
+            data (str, optional): Data of the new file. Defaults to None.
+        """
         f = open(file_path, "x")
-        if data is not None :
+        if data is not None:
             f.write(data)
         f.close()
 
-    @classmethod
-    def delete_file(self, file_path):
-        """ delete file """
+    def delete_file(self, file_path: str):
+        """
+        Delete the file.
 
+        Args:
+            file_path (str): Path to the file.
+        """
         os.remove(file_path)
 
-    
+
 if __name__ == '__main__':
     FH = FileHandler()
--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -3,65 +3,65 @@ import os
 
 from src.file_handler.file_handler import FileHandler
 
+
 class TestFileHandler(unittest.TestCase):
     """
     The class test methods from FileHandler class.
     """
 
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls):
         """ before all tests """
-
-        print("[START]  FileHandler tests")
-        self.directory = os.path.expanduser('./tmp_tests')
-        if not os.path.exists(self.directory):
-            os.makedirs(self.directory)
-
+        print('[START]  FileHandler tests')
+        cls.directory = os.path.expanduser('./tmp_tests')
+        if not os.path.exists(cls.directory):
+            os.makedirs(cls.directory)
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         """ after all tests """
-
-        print("[END]    FileHandler tests")
-        os.removedirs(self.directory)
-
+        print('[END]    FileHandler tests')
+        os.removedirs(cls.directory)
 
     @classmethod
-    def setUp(self):
+    def setUp(cls):
         """ before each test """
+        cls.FH = FileHandler()
 
-        self.FH = FileHandler()  
-
-    def test_create_delete_empty_file(self): 
-        file_path = os.path.join(self.directory, "test_create_delete_empty_file.txt")
+    def test_create_delete_empty_file(self):
+        file_path = os.path.join(self.directory,
+                                 'test_create_delete_empty_file.txt')
         self.FH.create_file(file_path)
         self.FH.delete_file(file_path)
 
-    def test_create_delete_file_with_content(self): 
-        file_path = os.path.join(self.directory, "test_create_delete_file_with_content.txt")
-        content = "content"
+    def test_create_delete_file_with_content(self):
+        file_path = os.path.join(
+            self.directory, 'test_create_delete_file_with_content.txt')
+        content = 'content'
         self.FH.create_file(file_path, content)
         result_content = self.FH.read_from_file(file_path)
-        self.assertMultiLineEqual(content, result_content)      
+        self.assertMultiLineEqual(content, result_content)
         self.FH.delete_file(file_path)
 
-    def test_create_write_delete(self): 
-        file_path = os.path.join(self.directory, "test_create_write_delete.txt")
-        data = ["content", "content_2"]
+    def test_create_write_delete(self):
+        file_path = os.path.join(
+            self.directory, 'test_create_write_delete.txt')
+        data = ['content', 'content_2']
         self.FH.create_file(file_path, data[0])
         self.FH.write_to_file(file_path, data[1])
         result_content = self.FH.read_from_file(file_path)
-        self.assertMultiLineEqual(data[1], result_content)      
-        self.FH.delete_file(file_path)    
+        self.assertMultiLineEqual(data[1], result_content)
+        self.FH.delete_file(file_path)
 
-    def test_create_add_delete(self): 
-        file_path = os.path.join(self.directory, "test_create_write_delete.txt")
-        data = ["content", "content_2"]
+    def test_create_add_delete(self):
+        file_path = os.path.join(
+            self.directory, 'test_create_write_delete.txt')
+        data = ['content', 'content_2']
         self.FH.create_file(file_path, data[0])
         self.FH.add_to_file(file_path, data[1])
         result_content = self.FH.read_from_file(file_path)
-        self.assertMultiLineEqual(''.join(data), result_content)      
-        self.FH.delete_file(file_path)  
+        self.assertMultiLineEqual(''.join(data), result_content)
+        self.FH.delete_file(file_path)
 
 
 if __name__ == '__main__':
