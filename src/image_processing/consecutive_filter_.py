80292933f53a66d7bec4bb2b6cf7c15ae986a2db
Martin Mrugala <martinmrugala@mbp-martin.home>
2020-11-28 13:38:33 +0100

    PEP8 failure v4

--- src/image_processing/consecutive_filter_.py
+++ src/image_processing/consecutive_filter_.py
@@ -5,16 +5,7 @@ from skimage.util import invert
 from tkinter import filedialog, Tk
 import os
 import sys
-from PIL import Image
 import math
-
-
-def PILcv2(image):
-    return Image.fromarray(image)
-
-
-def cv2PIL(image):
-    return np.asarray(image)
 
 
 def processPart(image, x1, y1, x2, y2):
@@ -24,66 +15,75 @@ def processPart(image, x1, y1, x2, y2):
         for x in range(x1, x2 + 1):
             point = []
             if image[x, y] == 0:
-                print(image[x, y])
                 point.append(x)
                 point.append(y)
                 points.append(point)
     return points
 
 
-def consecutive():
-    if len(sys.argv) == 1:
-        exit()
-
-    n = int(sys.argv[1])
+def get_dir_and_file():
     root = Tk()
     root.withdraw()
-    root.filename = filedialog.askopenfilename(
-        initialdir=".", title="Select file", filetypes=(("png files", "*.png"), ("all files", "*.*")))
-    path2 = os.path.splitext(os.path.split(root.filename)[1])[0]
+    root.filename = filedialog.askopenfilename(initialdir=".", title="Select file", filetypes=(
+        ("png files", "*.png"), ("all files", "*.*")))
     directory = os.path.dirname(root.filename)
+    path2 = os.path.splitext(os.path.split(root.filename)[1])[0]
     root.destroy()
+    return directory, path2, root.filename
 
-    img = cv2.imread(root.filename)
+
+def get_image(path):
+    img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     print(f"Size: {img.shape}")
+    return img
 
+
+def get_dimensions(img, n):
     width = img.shape[0]
     height = img.shape[1]
-    parts = []
     x_step = math.ceil(width / n)
     y_step = math.ceil(height / n)
-    i = 1
-    for y in range(0, height, y_step):
-        for x in range(0, width, x_step):
-            x1 = x
-            x2 = x + x_step - 1
-            if x2 >= width:
-                x2 = width - 1
-            y1 = y
-            y2 = y + y_step - 1
-            if y2 >= height:
-                y2 = height - 1
+    return width, height, x_step, y_step
 
-            print(
-                f"Square {i}:\n[{x1},{y1}]\t[{x2},{y1}]\n[{x1},{y2}]\t[{x2},{y2}]\n\n")
-            parts.append(processPart(img, x1, y1, x2, y2))
-            i += 1
-    i2 = 1
-    for part in parts:
-        print(f"Part: {i2}")
-        i2 += 1
-        for point in part:
-            print(f"Point: [{point[0]},{point[1]}]")
 
-    if len(sys.argv) == 2:
-        exit()
+def get_box(x, x_step, width, y, y_step, height):
+    x1 = x
+    x2 = x + x_step - 1
+    if x2 >= width:
+        x2 = width - 1
+    y1 = y
+    y2 = y + y_step - 1
+    if y2 >= height:
+        y2 = height - 1
+    return x1, x2, y1, y2
 
-    n = int(sys.argv[2])
+
+def prepare_blank_image(img):
     img2 = np.zeros(shape=img.shape)
     img2 = img_as_ubyte(img2)
     img2 = invert(img2)
+    return img2
 
+
+def resize_and_show_images(img, img2):
+    size = (500, 500)
+    img = cv2.resize(img, size)
+    img3 = cv2.resize(img2, size)
+    cv2.imshow('Original', img)
+    cv2.imshow('Limited', img3)
+    cv2.waitKey()
+
+
+def get_parts(img, n, parts):
+    width, height, x_step, y_step = get_dimensions(img, n)
+    for y in range(0, height, y_step):
+        for x in range(0, width, x_step):
+            x1, x2, y1, y2 = get_box(x, x_step, width, y, y_step, height)
+            parts.append(processPart(img, x1, y1, x2, y2))
+
+
+def filter_points(img2, parts, n):
     index = 0
     while(n > 0):
         if len(parts[index]) != 0:
@@ -97,14 +97,26 @@ def consecutive():
         index += 1
         index = index % len(parts)
 
-    size = (500, 500)
-    img = cv2.resize(img, size)
-    img3 = cv2.resize(img2, size)
-    cv2.imshow('Original', img)
-    cv2.imshow('Limited', img3)
 
-    cv2.waitKey()
+def consecutive():
+    if len(sys.argv) == 1:
+        exit()
+    n = int(sys.argv[1])
 
+    directory, path2, path = get_dir_and_file()
+
+    img = get_image(path)
+
+    parts = []
+    get_parts(img, n, parts)
+
+    if len(sys.argv) == 2:
+        exit()
+
+    n = int(sys.argv[2])
+    img2 = prepare_blank_image(img)
+    filter_points(img2, parts, n)
+    resize_and_show_images(img, img2)
     cv2.imwrite(directory + '/' + path2 + '_filtered.png', img2)
 
 
--- src/image_processing/random_filter.py
+++ src/image_processing/random_filter.py
@@ -5,17 +5,8 @@ from skimage.util import invert
 from tkinter import filedialog, Tk
 import os
 import sys
-from PIL import Image
 import math
 import random
-
-
-def PILcv2(image):
-    return Image.fromarray(image)
-
-
-def cv2PIL(image):
-    return np.asarray(image)
 
 
 def processPart(image, x1, y1, x2, y2):
@@ -24,73 +15,81 @@ def processPart(image, x1, y1, x2, y2):
         for x in range(x1, x2 + 1):
             point = []
             if image[x, y] == 0:
-                print(image[x, y])
                 point.append(x)
                 point.append(y)
                 points.append(point)
     return points
 
 
-def random_filter():
-    if len(sys.argv) == 1:
-        exit()
-
-    n = int(sys.argv[1])
+def get_dir_and_file():
     root = Tk()
     root.withdraw()
-    root.filename = filedialog.askopenfilename(initialdir="/Users/martinmrugala/Desktop/GP_III",
-                                               title="Select file", filetypes=(("png files", "*.png"), ("all files", "*.*")))
-    path2 = os.path.splitext(os.path.split(root.filename)[1])[0]
+    root.filename = filedialog.askopenfilename(initialdir=".", title="Select file", filetypes=(
+        ("png files", "*.png"), ("all files", "*.*")))
     directory = os.path.dirname(root.filename)
+    path2 = os.path.splitext(os.path.split(root.filename)[1])[0]
     root.destroy()
+    return directory, path2, root.filename
 
-    img = cv2.imread(root.filename)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print(f"Size: {img.shape}")
 
+def get_dimensions(img, n):
     width = img.shape[0]
     height = img.shape[1]
-    parts = []
     x_step = math.ceil(width / n)
     y_step = math.ceil(height / n)
-    i = 1
-    for y in range(0, height, y_step):
-        for x in range(0, width, x_step):
-            x1 = x
-            x2 = x + x_step - 1
-            if x2 >= width:
-                x2 = width - 1
-            y1 = y
-            y2 = y + y_step - 1
-            if y2 >= height:
-                y2 = height - 1
+    return width, height, x_step, y_step
 
-            print(
-                f"Square {i}:\n[{x1},{y1}]\t[{x2},{y1}]\n[{x1},{y2}]\t[{x2},{y2}]\n\n")
-            parts.append(processPart(img, x1, y1, x2, y2))
-            i += 1
-    i2 = 1
-    for part in parts:
-        print(f"Part: {i2}")
-        i2 += 1
-        for point in part:
-            print(f"Point: [{point[0]},{point[1]}]")
 
-    if len(sys.argv) == 2:
-        exit()
+def get_image(path):
+    img = cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print(f"Size: {img.shape}")
+    return img
 
-    n = int(sys.argv[2])
+
+def get_box(x, x_step, width, y, y_step, height):
+    x1 = x
+    x2 = x + x_step - 1
+    if x2 >= width:
+        x2 = width - 1
+    y1 = y
+    y2 = y + y_step - 1
+    if y2 >= height:
+        y2 = height - 1
+    return x1, x2, y1, y2
+
+
+def prepare_blank_image(img):
     img2 = np.zeros(shape=img.shape)
     img2 = img_as_ubyte(img2)
     img2 = invert(img2)
+    return img2
 
-    checked = np.zeros(sum(len(part) > 0 for part in parts))
+
+def resize_and_show_images(img, img2):
+    size = (500, 500)
+    img = cv2.resize(img, size)
+    img3 = cv2.resize(img2, size)
+    cv2.imshow('Original', img)
+    cv2.imshow('Limited', img3)
+    cv2.waitKey()
+
+
+def get_parts(img, n, parts):
+    width, height, x_step, y_step = get_dimensions(img, n)
+    for y in range(0, height, y_step):
+        for x in range(0, width, x_step):
+            x1, x2, y1, y2 = get_box(x, x_step, width, y, y_step, height)
+            parts.append(processPart(img, x1, y1, x2, y2))
+
+
+def filter_points(img2, parts, n,):
+    parts2 = [part for part in parts if len(part) > 0]
+    checked = np.zeros(len(parts2))
     n_checked = 0
     index = random.randint(0, len(checked) - 1 - n_checked)
-    parts2 = [part for part in parts if len(part) > 0]
 
     while(n > 0):
-        print(index)
         if len(parts2[index]) != 0:
             rand_index = random.randint(0, len(parts2[index]) - 1)
             img2[parts2[index][rand_index][0],
@@ -117,14 +116,27 @@ def random_filter():
             temp += 1
         index = temp
 
-    size = (500, 500)
-    img = cv2.resize(img, size)
-    img3 = cv2.resize(img2, size)
-    cv2.imshow('Original', img)
-    cv2.imshow('Limited', img3)
 
-    cv2.waitKey()
+def random_filter():
+    if len(sys.argv) == 1:
+        exit()
+    n = int(sys.argv[1])
 
+    directory, path2, path = get_dir_and_file()
+
+    img = get_image(path)
+
+    parts = []
+    get_parts(img, n, parts)
+
+    if len(sys.argv) == 2:
+        exit()
+
+    n = int(sys.argv[2])
+
+    img2 = prepare_blank_image(img)
+    filter_points(img2, parts, n)
+    resize_and_show_images(img, img2)
     cv2.imwrite(directory + '/' + path2 + '_filtered.png', img2)
 
 
