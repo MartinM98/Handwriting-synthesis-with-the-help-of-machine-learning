2fbe4c9331f4e2e6a9e4b64ddd6419ed5d398b1c
Martin Mrugala <martinmrugala@mbp-martin.home>
2020-11-29 16:41:51 +0100

    Controls points filtering - descriptions

--- src/image_processing/binary_search_filter.py
+++ src/image_processing/binary_search_filter.py
@@ -1,81 +1,24 @@
-import numpy as np
 import cv2
-from skimage import img_as_ubyte
-from skimage.util import invert
-from tkinter import filedialog, Tk
-import os
+from src.image_processing.common_functions.common_functions import get_dir_and_file
+from src.image_processing.common_functions.common_functions import get_parts
+from src.image_processing.common_functions.common_functions import get_image
+from src.image_processing.common_functions.common_functions import prepare_blank_image
+from src.image_processing.common_functions.common_functions import resize_and_show_images
 import sys
 import math
 import random
+import numpy as np
 
 
-def processPart(image, x1, y1, x2, y2):
-    points = []
-    for y in range(y1, y2 + 1):
-        for x in range(x1, x2 + 1):
-            point = []
-            if image[x, y] == 0:
-                point.append(x)
-                point.append(y)
-                points.append(point)
-    return points
-
-
-def get_dir_and_file():
-    root = Tk()
-    root.withdraw()
-    root.filename = filedialog.askopenfilename(initialdir=".", title="Select file", filetypes=(
-        ("png files", "*.png"), ("all files", "*.*")))
-    directory = os.path.dirname(root.filename)
-    path2 = os.path.splitext(os.path.split(root.filename)[1])[0]
-    root.destroy()
-    return directory, path2, root.filename
-
-
-def get_dimensions(img, n):
-    width = img.shape[0]
-    height = img.shape[1]
-    x_step = math.ceil(width / n)
-    y_step = math.ceil(height / n)
-    return width, height, x_step, y_step
-
-
-def get_image(path):
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print(f"Size: {img.shape}")
-    return img
-
-
-def get_box(x, x_step, width, y, y_step, height):
-    x1 = x
-    x2 = x + x_step - 1
-    if x2 >= width:
-        x2 = width - 1
-    y1 = y
-    y2 = y + y_step - 1
-    if y2 >= height:
-        y2 = height - 1
-    return x1, x2, y1, y2
-
-
-def prepare_blank_image(img):
-    img2 = np.zeros(shape=img.shape)
-    img2 = img_as_ubyte(img2)
-    img2 = invert(img2)
-    return img2
-
-
-def resize_and_show_images(img, img2):
-    size = (500, 500)
-    img = cv2.resize(img, size)
-    img3 = cv2.resize(img2, size)
-    cv2.imshow('Original', img)
-    cv2.imshow('Limited', img3)
-    cv2.waitKey()
-
-
-def add_end_points(halves, img2, parts2):
+def add_end_points(halves: list, img2: np.ndarray, parts2: list):
+    """
+    Adds first and last points/pixel to the filtered set of control points.
+    Args:
+       halves (list): list of selected points/pixels.
+       img2 (np.ndarray): image with filtered control points.
+       parts2 (list): list of subparts of the initial image that
+            that still contains control points.
+    """
     halves.append(0)
     rand_index = random.randint(0, len(parts2[0]) - 1)
     img2[parts2[0][rand_index][0], parts2[0][rand_index][1]] = 0
@@ -87,7 +30,20 @@ def add_end_points(halves, img2, parts2):
     parts2[len(parts2) - 1].pop(rand_index)
 
 
-def add_point(img2, tmp, halves, i, parts2, offset):
+def add_point(img2: np.ndarray, tmp: list, halves: list, i: int, parts2: list, offset: int):
+    """
+    Adds a point to the filtered set of control points.
+    Args:
+       img2 (np.ndarray): image with filtered control points.
+       tmp (list): Copy of the list of selected points/pixels
+           into which new point should be inserted.
+       halves (list): The list of selected points/pixels.
+       i (int): Index of the point that should be inserted.
+       parts2 (list): list of subparts of the initial image that
+            that still contains control points.
+       offset (int): offset in the tmp list (number of previously
+       inserted points compared to the halves list)
+    """
     mid = math.floor((halves[i + 1] - halves[i]) / 2)
     tmp.insert(i + 1 + offset, halves[i] + mid)
     rand_index = random.randint(
@@ -97,15 +53,15 @@ def add_point(img2, tmp, halves, i, parts2, offset):
     parts2[halves[i] + mid].pop(rand_index)
 
 
-def get_parts(img, n, parts):
-    width, height, x_step, y_step = get_dimensions(img, n)
-    for y in range(0, height, y_step):
-        for x in range(0, width, x_step):
-            x1, x2, y1, y2 = get_box(x, x_step, width, y, y_step, height)
-            parts.append(processPart(img, x1, y1, x2, y2))
-
-
-def filter_points(img2, parts, n):
+def filter_points(img2: np.ndarray, parts: list, n: int):
+    """
+    Main filtering function that saves the filtered
+    points/pixels in the img2 cv2 image.
+    Args:
+       img2 (np.ndarray): image with filtered control points.
+       parts (list): list of subparts of the initial image.
+       n (int): the number of subparts in each direction.
+    """
     parts2 = [part for part in parts if len(part) > 0]
     state = 2
     while(state < n):
@@ -129,6 +85,12 @@ def filter_points(img2, parts, n):
 
 
 def binary_search_filter():
+    """
+    Creates the subparts, filters the control points, shows the
+    results and saves the result.
+    Args:
+        None
+    """
     if len(sys.argv) == 1:
         exit()
     n = int(sys.argv[1])
--- src/image_processing/consecutive_filter_.py
+++ src/image_processing/consecutive_filter_.py
@@ -1,89 +1,22 @@
-import numpy as np
 import cv2
-from skimage import img_as_ubyte
-from skimage.util import invert
-from tkinter import filedialog, Tk
-import os
+from src.image_processing.common_functions.common_functions import get_dir_and_file
+from src.image_processing.common_functions.common_functions import get_parts
+from src.image_processing.common_functions.common_functions import get_image
+from src.image_processing.common_functions.common_functions import prepare_blank_image
+from src.image_processing.common_functions.common_functions import resize_and_show_images
 import sys
-import math
+import numpy as np
 
 
-def processPart(image, x1, y1, x2, y2):
-
-    points = []
-    for y in range(y1, y2 + 1):
-        for x in range(x1, x2 + 1):
-            point = []
-            if image[x, y] == 0:
-                point.append(x)
-                point.append(y)
-                points.append(point)
-    return points
-
-
-def get_dir_and_file():
-    root = Tk()
-    root.withdraw()
-    root.filename = filedialog.askopenfilename(initialdir=".", title="Select file", filetypes=(
-        ("png files", "*.png"), ("all files", "*.*")))
-    directory = os.path.dirname(root.filename)
-    path2 = os.path.splitext(os.path.split(root.filename)[1])[0]
-    root.destroy()
-    return directory, path2, root.filename
-
-
-def get_image(path):
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print(f"Size: {img.shape}")
-    return img
-
-
-def get_dimensions(img, n):
-    width = img.shape[0]
-    height = img.shape[1]
-    x_step = math.ceil(width / n)
-    y_step = math.ceil(height / n)
-    return width, height, x_step, y_step
-
-
-def get_box(x, x_step, width, y, y_step, height):
-    x1 = x
-    x2 = x + x_step - 1
-    if x2 >= width:
-        x2 = width - 1
-    y1 = y
-    y2 = y + y_step - 1
-    if y2 >= height:
-        y2 = height - 1
-    return x1, x2, y1, y2
-
-
-def prepare_blank_image(img):
-    img2 = np.zeros(shape=img.shape)
-    img2 = img_as_ubyte(img2)
-    img2 = invert(img2)
-    return img2
-
-
-def resize_and_show_images(img, img2):
-    size = (500, 500)
-    img = cv2.resize(img, size)
-    img3 = cv2.resize(img2, size)
-    cv2.imshow('Original', img)
-    cv2.imshow('Limited', img3)
-    cv2.waitKey()
-
-
-def get_parts(img, n, parts):
-    width, height, x_step, y_step = get_dimensions(img, n)
-    for y in range(0, height, y_step):
-        for x in range(0, width, x_step):
-            x1, x2, y1, y2 = get_box(x, x_step, width, y, y_step, height)
-            parts.append(processPart(img, x1, y1, x2, y2))
-
-
-def filter_points(img2, parts, n):
+def filter_points(img2: np.ndarray, parts: list, n: int):
+    """
+    Main filtering function that saves the filtered
+    points/pixels in the img2 cv2 image.
+    Args:
+       img2 (np.ndarray): image with filtered control points.
+       parts (list): list of subparts of the initial image.
+       n (int): the number of subparts in each direction.
+    """
     index = 0
     while(n > 0):
         if len(parts[index]) != 0:
@@ -99,6 +32,12 @@ def filter_points(img2, parts, n):
 
 
 def consecutive():
+    """
+    Creates the subparts, filters the control points, shows the
+    results and saves the result.
+    Args:
+        None
+    """
     if len(sys.argv) == 1:
         exit()
     n = int(sys.argv[1])
