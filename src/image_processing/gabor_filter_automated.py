5855b0cb9f607e9cd18666117029caa3a211bfca
Martin Mrugala <martinmrugala@mbp-martin.home>
2020-12-04 18:45:30 +0100

    Gabor filter functions

--- src/image_processing/gabor_filter_automated.py
+++ src/image_processing/gabor_filter_automated.py
@@ -3,39 +3,52 @@ import cv2
 from skimage.filters import gabor
 from skimage import img_as_ubyte
 from skimage.util import invert
-from src.file_handler.file_handler import get_file_name
+from src.file_handler.file_handler import get_filename_without_extention
 from src.image_processing.common_functions.common_functions import get_dir
+from src.image_processing.common_functions.common_functions import get_image
+from src.image_processing.common_functions.common_functions import prepare_blank_image
 import os
 
 
-def gabor_filter_automated():
+def gabor_filter_automated(directory: str = None):
     """
     Applies the gabor filter with given paramters to
     all images with the extension png in the selected directory and
     saves the results.
+
     Args:
-       None
+       directory (str, optional): the path to the directory that should
+       be processed.
+
+    Returns:
+        list: The list of filtered images.
     """
-    directory = get_dir()
+    save = False
+    if directory is None:
+        directory = get_dir()
+        save = True
+
+    results = []
 
     thetas = [0.25 * np.pi, 0.75 * np.pi]
     lamda = 0.16 * np.pi
     for entry in os.scandir(directory):
-        if (entry.path.endswith(".png")):
-            path2 = get_file_name(entry.path)
+        if (entry.path.endswith("_skel.png")):
+            path2 = get_filename_without_extention(entry.path)
             path = entry.path
-            img = cv2.imread(path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            result = np.zeros(shape=img.shape)
-            result = img_as_ubyte(result)
-            result = invert(result)
+            img = get_image(path)
+            result = prepare_blank_image(img.shape)
             for theta in thetas:
                 filt_real, filt_imag = gabor(
                     img, frequency=1 / lamda, theta=theta)
                 cv_image = img_as_ubyte(filt_imag)
                 cv_image = invert(cv_image)
                 result = np.bitwise_and(result, cv_image)
-            cv2.imwrite(directory + '/' + path2 + '_control_points.png', result)
+            if save:
+                cv2.imwrite(directory + '/' + path2 + '_control_points.png', result)
+            results.append(result)
+
+    return results
 
 
 if __name__ == '__main__':
