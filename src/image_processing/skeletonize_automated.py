d6c012c3f14da71c467060e9a97814e0bb4416ca
Martin Mrugala <martinmrugala@mbp-martin.home>
2020-12-04 18:45:43 +0100

    Skeletonization functions

--- src/image_processing/skeletonize_automated.py
+++ src/image_processing/skeletonize_automated.py
@@ -6,22 +6,32 @@ from skimage.util import invert
 import cv2
 import numpy as np
 import os
-from src.file_handler.file_handler import get_file_name
+from src.file_handler.file_handler import get_filename_without_extention
 from src.image_processing.common_functions.common_functions import get_dir
 
 
-def skeletonize_automated():
+def skeletonize_automated(directory: str = None):
     """
     Skeletonizes all images with the extension png in the selected directory and
     saves the results.
+
     Args:
-       None
+       directory (str, optional): the path to the directory that should
+       be processed.
+
+    Returns:
+        list: The list of skeletonized images.
     """
-    directory = get_dir()
+    save = False
+    if directory is None:
+        directory = get_dir()
+        save = True
+
+    results = []
 
     for entry in os.scandir(directory):
         if (entry.path.endswith(".png")):
-            path2 = get_file_name(entry.path)
+            path2 = get_filename_without_extention(entry.path)
             path = entry.path
             image2 = io.imread(path)
             image2 = invert(image2)
@@ -37,7 +47,11 @@ def skeletonize_automated():
                 if(image4[ix, iy] != 255):
                     image4[ix, iy] = 0
             _, img = cv2.threshold(image4, 2, 255, cv2.THRESH_BINARY)
-            plt.imsave(directory + '/' + path2 + '_skel.png', img, cmap=plt.cm.gray)
+            if save:
+                plt.imsave(directory + '/' + path2 + '_skel.png', img, cmap=plt.cm.gray)
+            results.append(img)
+
+    return results
 
 
 if __name__ == '__main__':
