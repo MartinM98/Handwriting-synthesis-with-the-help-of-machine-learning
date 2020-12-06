6d88d3a5f168ff0c25e281441e7879ab1c311379
zbartkus <zbartkuss3150@gmail.com>
2020-12-06 17:08:46 +0100

    comment cut

--- src/synthesis/pix2pix.py
+++ src/synthesis/pix2pix.py
@@ -249,8 +249,6 @@ def load_examples():
         name, _ = os.path.splitext(os.path.basename(path))
         return name
 
-    # if the image names are numbers, sort by the value rather than asciibetically
-    # having sorted inputs means that the outputs are sorted in test mode
     if all(get_name(path).isdigit() for path in input_paths):
         input_paths = sorted(input_paths, key=lambda path: int(get_name(path)))
     else:
@@ -270,13 +268,11 @@ def load_examples():
         raw_input.set_shape([None, None, 3])
 
         if a.lab_colorization:
-            # load color and brightness from image, no B image exists here
             lab = rgb_to_lab(raw_input)
             L_chan, a_chan, b_chan = preprocess_lab(lab)
             a_images = tf.expand_dims(L_chan, axis=2)
             b_images = tf.stack([a_chan, b_chan], axis=2)
         else:
-            # break apart image pair and move to range [-1, 1]
             width = tf.shape(raw_input)[1]  # [height, width, channels]
             a_images = preprocess(raw_input[:, :width // 2, :])
             b_images = preprocess(raw_input[:, width // 2:, :])
