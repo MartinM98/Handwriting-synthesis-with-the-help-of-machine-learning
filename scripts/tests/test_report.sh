41c62f2e8b0de7f9a887bff00700607bda9d1665
Patryk <patryk.wal@onet.pl>
2020-12-06 16:43:52 +0100

    fix coverage

--- scripts/tests/test_report.sh
+++ scripts/tests/test_report.sh
@@ -1,4 +1,4 @@
-coverage run -m --rcfile="scripts/tests/.coveragerc" pytest "tests/run_all_tests.py"
+coverage run -m --rcfile="scripts/tests/.coveragerc" --source="src" pytest "tests/run_all_tests.py"
 coverage report --rcfile="scripts/tests/.coveragerc"
 coverage html --rcfile="scripts/tests/.coveragerc"
 coverage erase
