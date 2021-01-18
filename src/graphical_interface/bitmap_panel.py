96bd89ba5f24d4b98d6ac3e677e4da0ad62af3d3
Martin <m.mrugala@student.mini.pw.edu.pl>
2021-01-18 21:33:23 +0100

    Synthesis panel - Bitmap allignment for MacOS

--- src/graphical_interface/bitmap_panel.py
+++ src/graphical_interface/bitmap_panel.py
@@ -6,9 +6,10 @@ class BitmapPanel(scrl.ScrolledPanel):
     def __init__(self, parent, bitmap):
         scrl.ScrolledPanel.__init__(self, parent)
 
-        self.mainSizer = wx.BoxSizer(wx.VERTICAL)
-        self.imageCtrl = wx.StaticBitmap(self, wx.ID_ANY,
-                                         bitmap=bitmap)
-        self.mainSizer.Add(self.imageCtrl, 1, wx.EXPAND)
-        self.SetSizerAndFit(self.mainSizer)
+        self.mainSizer_v = wx.BoxSizer(wx.VERTICAL)
+        self.mainSizer_h = wx.BoxSizer(wx.HORIZONTAL)
+        self.imageCtrl = wx.StaticBitmap(self, wx.ID_ANY, bitmap=bitmap)
+        self.mainSizer_v.Add(self.imageCtrl, 1, wx.CENTER)
+        self.mainSizer_h.Add(self.mainSizer_v, 1, wx.CENTER)
+        self.SetSizerAndFit(self.mainSizer_h)
         self.SetupScrolling()
